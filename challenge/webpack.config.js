4aea5cf5098079ab11a46a01a876432835025dca
Luis Loyola <luis@MacBook-Air-de-Luis.local>
2022-07-11 11:18:24 -0400

    Fix production build issues

--- challenge/webpack.config.js
+++ challenge/webpack.config.js
@@ -1,13 +1,19 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: path.resolve(__dirname, "./src/index.jsx"),
+  entry: path.resolve(__dirname, "src", "index.jsx"),
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
+        include: path.resolve(__dirname, "src"),
         use: ["babel-loader"]
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
       }
     ]
   },
@@ -15,10 +21,18 @@ module.exports = {
     extensions: ["*", ".js", ".jsx"]
   },
   output: {
-    path: path.resolve(__dirname, "./public"),
-    filename: "bundle.js"
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true
   },
   devServer: {
-    static: path.resolve(__dirname, "./public")
-  }
+    static: path.join(__dirname, "dist")
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+      inject: true,
+      filename: "index.html"
+    })
+  ]
 };
