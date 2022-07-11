60ce381a600ee0a8f2a73cf0c6a23118882bc9b4
Luis Loyola <luis@MacBook-Air-de-Luis.local>
2022-07-11 09:21:45 -0400

    Set up eslint & prettier

--- challenge/src/App.jsx
+++ challenge/src/App.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import React from "react";
+// import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-const client = new ApolloClient({
-  uri: 'https://countries.trevorblades.com',
-  cache: new InMemoryCache()
-});
+// const client = new ApolloClient({
+//   uri: "https://countries.trevorblades.com",
+//   cache: new InMemoryCache(),
+// });
 
-const Results = () => <p>First try</p>;
+function Results() {
+  return <p>First try</p>;
+}
 
 export default Results;
--- challenge/webpack.config.js
+++ challenge/webpack.config.js
@@ -1,26 +1,24 @@
-const webpack = require("webpack");
 const path = require("path");
 
 module.exports = {
-  entry: path.resolve(__dirname, "./src/index.js"),
+  entry: path.resolve(__dirname, "./src/index.jsx"),
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ["babel-loader"],
-      },
-    ],
+        use: ["babel-loader"]
+      }
+    ]
   },
   resolve: {
-    extensions: ["*", ".js", ".jsx"],
+    extensions: ["*", ".js", ".jsx"]
   },
   output: {
     path: path.resolve(__dirname, "./public"),
-    filename: "bundle.js",
+    filename: "bundle.js"
   },
-  plugins: [new webpack.HotModuleReplacementPlugin()],
   devServer: {
     static: path.resolve(__dirname, "./public")
-  },
+  }
 };
