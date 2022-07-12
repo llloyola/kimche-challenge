8c22c2192c2000c0c8925293d56aa386dd8bf46a
Luis Loyola <luis@MacBook-Air-de-Luis.local>
2022-07-12 11:50:44 -0400

    add title and fixed minor issues

--- challenge/src/App.jsx
+++ challenge/src/App.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { useQuery } from "@apollo/client";
-import groupResults from "./utils/groupResults";
+// import groupResults from "./utils/groupResults";
 import GET_COUNTRIES from "./queries/getCountries";
+import Title from "./components/Title/Title";
 
 function App() {
-	const { loading, error, data } = useQuery(GET_COUNTRIES);
+	const { loading, error } = useQuery(GET_COUNTRIES);
 
 	if (loading) return <p>Loading...</p>;
 	if (error) return <p>Error :</p>;
 
-	return <p>{JSON.stringify(groupResults(data.countries, true))}</p>;
+	return <Title title="Country Search" />;
 }
 
 export default App;
